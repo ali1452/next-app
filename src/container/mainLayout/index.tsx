b6fbb931ab04e9f64f7de0f3a61f47742dbfc378
ali1452 <aliabbas.mkting@gmail.com>
2023-12-01 11:47:59 +0500

    product cards

--- src/container/mainLayout/index.tsx
+++ src/container/mainLayout/index.tsx
@@ -2,19 +2,45 @@
 
 import React from 'react'
 import  style from './mainLayout.module.scss'
+import { productData } from './productdata'
+import Image from 'next/image'
 
 const MainLayout = ({data}:any) => {
  
- console.log('data',1)
+ console.log('data',productData)
     
   return (
     <div className={style.main_layout_container}>
-        <h1>Client List</h1>
+        {/* <h1>Client List</h1>
         {data && data.map((item:any)=>{
             return(
                 <p onClick={()=>console.log('hello')} className={style.customer_name} key={item._id}>{item.first_name} {item.last_name}</p>
             )
+        })} */}
+        {productData  &&  productData.length > 0 ?
+        <>
+        {productData.map((item,i)=>{
+          const {url,name,price,sku} = item
+          return(
+            <div key={"product" + i} className={style.card}>
+            <img src={url} alt='Product Image' width='400px' height='500px' />
+            <p className={style.product_name}>{name}</p>
+            <p className={style.price}>Rs.{price}</p>
+            <span className={style.sku}>
+              Size:
+            {sku.map((item)=>{
+              return(
+                <p style={{textTransform:'uppercase'}}>{item}</p>
+              )
+            })}
+            </span>
+
+            </div>
+          )
         })}
+        </>
+        
+        :<h1>No Product Avaialable</h1>}
     </div>
   )
 }
---
+++ src/container/mainLayout/productdata.ts
@@ -0,0 +1,103 @@
+export const productData =  [
+    {
+        name:'Off-White Basic Tee MN-TS-BS-WS23-001',
+        price:'1500',
+        category:'men',
+        brand:'zeen',
+        description:'men T-shirt for winter season',
+        url:'black_basic_tee_mn-ts-bs-ws23-003_2_.webp',
+        discount_price:'1250',
+        sku:['s','m','l']
+    },
+    {
+        name:'Black Basic Tee MN-TS-BS-WS23-003',
+        price:'1500',
+        category:'men',
+        brand:'zeen',
+        description:'men T-shirt for winter season',
+        url:'coffee_basic_tee_mn-ts-bs-ws23-004_2_.webp',
+        discount_price:'1350',
+        sku:['s','m','l']
+    },
+    {
+        name:' Coffee Basic Tee MN-TS-BS-WS23-004',
+        price:'1600',
+        category:'men',
+        brand:'zeen',
+        description:'men T-shirt for winter season',
+        url:'expresso_basic_tee_mn-ts-bs-ws23-006_1_.webp',
+        discount_price:'1350',
+        sku:['m','l']
+    },
+    {
+        name:'Expresso Basic Tee MN-TS-BS-WS23-006',
+        price:'1500',
+        category:'men',
+        brand:'zeen',
+        description:'men T-shirt for winter season',
+        url:'latte_basic_tee_mn-ts-bs-ws23-007_2_.webp',
+        discount_price:'1350',
+        sku:['s','m','l']
+    },
+    {
+        name:'Latte Basic Tee MN-TS-BS-WS23-007',
+        price:'1500',
+        category:'men',
+        brand:'zeen',
+        description:'men T-shirt for winter season',
+        url:'off-white_basic_tee_mn-ts-bs-ws23-001_2_.webp',
+        discount_price:'1250',
+        sku:['s','m','l']
+    },
+    {
+        name:'Off-White Basic Tee MN-TS-BS-WS23-001',
+        price:'1500',
+        category:'men',
+        brand:'zeen',
+        description:'men T-shirt for winter season',
+        url:'black_basic_tee_mn-ts-bs-ws23-003_2_.webp',
+        discount_price:'1250',
+        sku:['s','m','l']
+    },
+    {
+        name:'Black Basic Tee MN-TS-BS-WS23-003',
+        price:'1500',
+        category:'men',
+        brand:'zeen',
+        description:'men T-shirt for winter season',
+        url:'coffee_basic_tee_mn-ts-bs-ws23-004_2_.webp',
+        discount_price:'1350',
+        sku:['s','m','l']
+    },
+    {
+        name:' Coffee Basic Tee MN-TS-BS-WS23-004',
+        price:'1600',
+        category:'men',
+        brand:'zeen',
+        description:'men T-shirt for winter season',
+        url:'expresso_basic_tee_mn-ts-bs-ws23-006_1_.webp',
+        discount_price:'1350',
+        sku:['m','l']
+    },
+    {
+        name:'Expresso Basic Tee MN-TS-BS-WS23-006',
+        price:'1500',
+        category:'men',
+        brand:'zeen',
+        description:'men T-shirt for winter season',
+        url:'latte_basic_tee_mn-ts-bs-ws23-007_2_.webp',
+        discount_price:'1350',
+        sku:['s','m','l']
+    },
+    {
+        name:'Latte Basic Tee MN-TS-BS-WS23-007',
+        price:'1500',
+        category:'men',
+        brand:'zeen',
+        description:'men T-shirt for winter season',
+        url:'off-white_basic_tee_mn-ts-bs-ws23-001_2_.webp',
+        discount_price:'1250',
+        sku:['s','m','l']
+    },
+
+]
