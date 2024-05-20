66ae7591bc87f43a8c82975223f64dfde656de11
ali1452 <aliabbas.mkting@gmail.com>
2024-05-21 03:04:28 +0500

    add search functionality

--- src/component/header/index.tsx
+++ src/component/header/index.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 import style from './header.module.scss'
 import Link from 'next/link'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SearchIcon from '@mui/icons-material/Search';
 import { useSelector } from 'react-redux';
+import SearchBox from '../searcrch-box/search-box-index';
 
 const Header = () => {
   const count= useSelector((item:any)=>item.cart.cart)
@@ -18,12 +20,18 @@ const Header = () => {
     <div className={style.header_container}>
          <Link href='/'><p>M&B</p></Link>
         <p>All Your Needs</p>
+        <div className={style.icon_wrap} >
+        <p className={style.search_icon}>
+          {/* <SearchIcon /> */}
+          <SearchBox />
+          </p>
         <p>
         <Link href='/cart'>
           <span className={style.cart_icon}><ShoppingCartIcon  />
           {total_item >0?<span className={style.count}>{total_item}</span>:""}
           </span></Link>
         </p>
+        </div>
         </div>
   )
 }
---
+++ src/component/searcrch-box/search-box-index.tsx
@@ -0,0 +1,133 @@
+"use client"
+
+import React, { useEffect, useState } from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import SearchIcon from '@mui/icons-material/Search';
+import styles from './search-box.module.scss'
+import { getAllProducts } from '@/services/userservices';
+import Link from 'next/link';
+
+
+type Props = {}
+
+type Anchor = 'top' | 'left' | 'bottom' | 'right';
+
+const SearchBox = (props: Props) => {
+    const [product,setProduct] = useState([])
+    const [state, setState] = useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+      });
+
+      const [search, setSearch] =useState('')
+      const [selectedProduct,setSelectedProduct] = useState([])
+
+      const fetchProduct = async()=>{
+        const res = await getAllProducts()
+        if(res?.status == 200){
+            setProduct(res.data)
+        }
+       
+      }
+
+      useEffect(()=>{
+        fetchProduct()
+      },[])
+
+      const toggleDrawer = (anchor: Anchor, open: boolean) =>
+      (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (
+          event.type === 'keydown' &&
+          ((event as React.KeyboardEvent).key === 'Tab' ||
+            (event as React.KeyboardEvent).key === 'Shift')
+        ) {
+          return;
+        }
+  
+        setState({ ...state, [anchor]: open });
+      };
+
+      const searchProduct =(e: { target: { value: React.SetStateAction<string>; }; })=>{
+        setSearch(e.target.value)
+        const key_words = e.target.value
+        if(key_words !==""){
+          const  selectedItems = product.filter((item:any)=>{
+            if(item.name.toLowerCase().includes(key_words)){
+              console.log(item)
+              return item
+  
+            }
+          })
+          setSelectedProduct(selectedItems)
+  
+        }else{
+          setSelectedProduct([])
+        }
+        
+      }
+  
+    const list = (anchor: Anchor,styles: { readonly [x: string]: string; search_input?: any; }) => (
+      <div
+        role="presentation"
+        // onClick={toggleDrawer(anchor, false)}
+        // onKeyDown={toggleDrawer(anchor, false)}
+        style={{marginTop:'4%'}}
+      >
+        <>
+        <p style={{margin:'10px'}}>
+          <input  value={search} className={styles.search_input} type='text' onChange={(e)=>searchProduct(e)} />
+        </p>
+        <div style={{display:'flex', flexWrap:'wrap', justifyContent:'center'}}>
+          {selectedProduct.map((item:any, index:number) => {
+            return(
+            <Link href={`/products/${item.product_id}`} onClick={()=>setState({...state,top:false})}>
+            <div key={item} style={{padding:'10px'}}>
+              <p>
+                <img src={`../${item.url}`} alt='image' />
+              </p>
+              <p>{item.name}</p>
+            </div>
+            </Link>
+            )
+            
+            })}
+        </div>
+        </>
+      </div>
+    );
+  
+  return (
+    <>
+    <div className={styles.search_container}>
+
+          {(['top'] as const).map((anchor) => (
+            
+        <React.Fragment key={anchor}>
+          <Button className={styles.btn} onClick={toggleDrawer(anchor, !state.top)}>{<SearchIcon />}</Button>
+          <Drawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+          >
+            {list(anchor,styles)}
+          </Drawer>
+        </React.Fragment>
+      ))}
+    </div>
+    </>
+  )
+}
+
+export default SearchBox
