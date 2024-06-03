c14ea94f22dbdf1dd156e4b57fc7ac75ceb83729
ali1452 <aliabbas.mkting@gmail.com>
2024-06-03 12:00:21 +0500

    add config

--- src/globalConfig.ts
+++ src/globalConfig.ts
@@ -3,12 +3,14 @@ const Environment = process.env.NODE_ENV
 
 type GconfigType = {
     api_url:string,
-    image_url:string
+    image_url:string,
+    environment:string
 }
 
 let  Gconfig:GconfigType = {
     api_url:'',
-    image_url:''
+    image_url:'',
+    environment:''
 }
 const API_URL = "https://express-project-smoky.vercel.app"
 const IMAGE_URL = "https://res.cloudinary.com/dpnza2tuy/image/upload/v1717048876/"
@@ -18,14 +20,16 @@ const IMAGE_URL = "https://res.cloudinary.com/dpnza2tuy/image/upload/v1717048876
 if(Environment == 'development'){
      Gconfig = {
         api_url:API_URL,
-        image_url:IMAGE_URL
+        image_url:IMAGE_URL,
+        environment:Environment
     }
 }
 
 if(Environment == 'production'){
     Gconfig = {
        api_url:API_URL,
-       image_url:IMAGE_URL
+       image_url:IMAGE_URL,
+       environment:Environment
    }
 }
 
