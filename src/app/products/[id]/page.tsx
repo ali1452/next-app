d8c3297aabccf9179cdbd50442e61f5975500a1a
ali1452 <aliabbas.mkting@gmail.com>
2025-07-26 00:52:08 +0500

    refactor: update ProductDetail component to use data prop instead of id

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ProductDetailComp from '@/container/productDetailComp';
+import { getProduct } from '@/services/userservices'
 
-const ProductDetail = ({params}:any) => {
-    console.log({params})
+const ProductDetail = async({params}:any) => {
+  let data = null
+  const res = await getProduct(params.id)
+
+  if(res?.status== 200){
+    data = res.data[0]
+  }
+
   return (
     <div>
-      <ProductDetailComp id={params.id} />
+      <ProductDetailComp data={data} />
     </div>
   )
 }
